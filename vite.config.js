3b1fe1e51fd42a8e40e1a74c9a26490f2eb50cdd
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-12-18 19:43:36 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,30 @@
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   build: {
-//     sourcemap: true,
-//     alias: {
-//       "@": "/src" // Alias dla folderu `src`
-//     }
-//   }
-// });
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/user': {
-        target: 'http://localhost:3000', // Adres backendu
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
+  build: {
+    sourcemap: true,
+    alias: {
+      "@": "/src" // Alias dla folderu `src`
+    }
+  }
 });
+
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/user': {
+//         target: 'http://localhost:3000', // Adres backendu
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+// });
