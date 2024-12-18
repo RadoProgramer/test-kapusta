3b68c4bd4fa91ba8520a9e66c0d12165cf0db549
RadoProgramer <145141434+RadoProgramer@users.noreply.github.com>
2024-12-18 19:54:18 +0000

    bb

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,6 @@
-// const API_URL = import.meta.env.VITE_API_URL || "https://kapusta-backend.goit.global";
-// export default API_URL;
-
-
-const API_URL = import.meta.env.VITE_API_URL || "https://be-kapusta-team-project.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://kapusta-backend.goit.global";
 export default API_URL;
+
+
+// const API_URL = import.meta.env.VITE_API_URL || "https://be-kapusta-team-project.onrender.com";
+// export default API_URL;
